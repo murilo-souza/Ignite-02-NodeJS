80acd5b50cb393cc8eb9a4632b04748a0857ba2c
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-12-28 21:21:01 -0300

    Module 2 class 4 - Realiazando queries com Knex

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return tables
+  return transaction
 })
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('HTTP Server Runninwwrrrrwg')
+  console.log('HTTP Server Running')
 })
