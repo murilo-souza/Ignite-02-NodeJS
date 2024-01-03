652bf2d14988215db8fd85e7c4f722d2c89edf01
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-01-02 21:01:50 -0300

    Production config

--- src/database.ts
+++ src/database.ts
@@ -12,7 +12,7 @@ export const config: Knex.Config = {
       ? {
           filename: env.DATABASE_URL,
         }
-      : env.DATABASE_CLIENT,
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
